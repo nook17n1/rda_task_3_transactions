cfe7ae6b9ab2b004548ff16c3a04927afe8d2d7b
Alexandr Titarenko <nook17n1@gmail.com>
2026-05-26 13:37:39 +0300

    Solution2

--- task.sql
+++ task.sql
@@ -3,11 +3,10 @@ USE ShopDB;
 
 -- Some data should be created outside the transaction (here)
 
-
--- Start the transaction 
-START TRANSACTION; 
 INSERT INTO Orders (CustomerID, Date)
     VALUES (1, "2023-01-01"); 
+-- Start the transaction 
+START TRANSACTION; 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
     VALUES (LAST_INSERT_ID(), 1, 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 where Name = "AwersomeProduct";
