75139614f6408dfed10384570ef1b0dcc872e416
Alexandr Titarenko <nook17n1@gmail.com>
2026-05-26 13:25:24 +0300

    Solution1

--- task.sql
+++ task.sql
@@ -8,8 +8,7 @@ INSERT INTO Orders (CustomerID, Date)
 -- Start the transaction 
 START TRANSACTION; 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-    VALUES (1, 1, 0);
-UPDATE OrderItems SET Count = Count + 1 WHERE OrderID = 1;
+    VALUES (LAST_INSERT_ID(), 1, 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 where Name = "AwersomeProduct";
 -- And some data should be created inside the transaction 
 
